fb205fc1df4813c41b0e17fa5ff4f73ede384ab2
Alan <yashikalana@mail.ru>
2025-12-01 14:18:16 +0300

    Initial Commit

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Алан Парастаев on 01.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
